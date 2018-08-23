e9aac78c36faa28f1f877597002ed880a690be3a
YDanot <y.danot@gmail.com>
2018-08-23 17:45:07 +0200

    refactor tests to use wallet everytime

--- src/test/java/wallet/WalletTest.java
+++ src/test/java/wallet/WalletTest.java
@@ -12,30 +12,31 @@ import static wallet.StockType.US_DOLLAR;
 
 public class WalletTest {
 
-    private Stock btcStock = Stock.of(BigDecimal.ZERO, BITCOIN);
-    private Stock usdStock = Stock.of(BigDecimal.ZERO, US_DOLLAR);
-
     private Rate btcEurRate;
     private Rate usdEurRate;
 
     private Money finalValue;
+
     private WalletBuilder walletBuilder = WalletBuilder.aWallet();
     private Wallet wallet;
-    ;
+
 
     @Test
     public void should_compute_value_in_EUR() throws IOException {
-        given_a_BTC_stock_of(2);
+        given_a_wallet()
+                .withStock(Stock.of(BigDecimal.valueOf(2), BITCOIN));
         and_BTC_to_EUR_rate_is(5000);
         when_I_compute_value_in_euro();
         computed_euro_value_should_be(10000);
 
-        given_a_BTC_stock_of(2);
+        given_a_wallet()
+                .withStock(Stock.of(BigDecimal.valueOf(2), BITCOIN));
         and_BTC_to_EUR_rate_is(2000);
         when_I_compute_value_in_euro();
         computed_euro_value_should_be(4000);
 
-        given_a_USD_stock_of(10);
+        given_a_wallet()
+                .withStock(Stock.of(BigDecimal.valueOf(10), US_DOLLAR));
         and_USD_to_EUR_rate_is(0.90);
         when_I_compute_value_in_euro();
         computed_euro_value_should_be(9);
@@ -59,16 +60,8 @@ public class WalletTest {
         return walletBuilder;
     }
 
-    private void given_a_BTC_stock_of(int value) {
-        btcStock = Stock.of(BigDecimal.valueOf(value), BITCOIN);
-    }
-
     private void and_BTC_to_EUR_rate_is(int value) {
         btcEurRate = Rate.of(BigDecimal.valueOf(value), BITCOIN, EUR);
-    }
-
-    private void given_a_USD_stock_of(int value) {
-        usdStock = Stock.of(BigDecimal.valueOf(value), US_DOLLAR);
     }
 
     private void and_USD_to_EUR_rate_is(double rate) {
@@ -82,12 +75,6 @@ public class WalletTest {
     }
 
     private Money computeValue() {
-        if (usdStock.value().doubleValue() > 0) {
-            return Money.of(usdEurRate.apply(usdStock.value()), EUR);
-        } else if (btcStock.value().doubleValue() > 0) {
-            return Money.of(btcEurRate.apply(btcStock.value()), EUR);
-        }
-
         Money money = Money.of(BigDecimal.ZERO, EUR);
         for (Stock stock : wallet.stockList()) {
             if (US_DOLLAR.equals(stock.stockType())) {
